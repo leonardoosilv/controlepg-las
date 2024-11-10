f5920f46dc18dbba840d588efe357970adbafdff
leonardoosilv <82114638+leonardoosilv@users.noreply.github.com>
2024-11-10 16:17:27 -0300

    Update login.js
    add config para supabase

--- login.js
+++ login.js
@@ -1,4 +1,9 @@
-    // Função de validação de login
+// Configuração do Supabase
+const supabaseUrl = 'https://iltlyenxlkegbwoalihn.supabase.co';  // Substitua pelo URL do seu Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsdGx5ZW54bGtlZ2J3b2FsaWhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMDg2MzUsImV4cCI6MjA0Njc4NDYzNX0.kku-h687kIvvfAWycDE1OxT6TValyBSJbxxGpeXk4l4';  // Substitua pela chave pública do Supabase
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+// Função de validação de login
     function validarLogin() {
         const usuario = document.getElementById('usuario').value;
         const senha = document.getElementById('senha').value;
